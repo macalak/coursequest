3f25c984b66633a16eae872d9607168689183d2b
pittner <lubos.pittner@posam.sk>
2017-02-06 00:16:31 +0100

    init commit

---
+++ coursequest-rest/src/main/java/sk/posam/coursequest/CourseQuestApplication.java
@@ -0,0 +1,12 @@
+package sk.posam.coursequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseQuestApplication.class, args);
+	}
+}
